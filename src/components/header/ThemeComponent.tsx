15eae90a642dd90c63f05e5bca2e62b1d2a307f0
Rogerio-N <rogenakayama@gmail.com>
2025-09-21 17:47:04 -0300

    Create send email modal

--- src/components/header/ThemeComponent.tsx
+++ src/components/header/ThemeComponent.tsx
@@ -23,7 +23,7 @@ export default function ThemeComponent() {
     const isThemeLightActive = theme === "light"
 
     return (
-        <button onClick={handleThemeChange}>
+        <button className="hover:cursor-pointer" onClick={handleThemeChange}>
             {isThemeLightActive ? <SunIcon className="w-6 sm:w-8 h-full align-middle"/> : <MoonIcon className="w-5 h-full align-middle rotate-270 sm:w-6"/>}
         </button>
     )
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -5,10 +5,12 @@ import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 import Skills from "./skills";
 import Projects from "./projects";
+import SendEmailModal from "./sendEmailModal";
 
 export default function Main() {
 
     const [mounted, setMounted] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const translator = useTranslations('Home')
         
     useEffect(() => {
@@ -29,12 +31,19 @@ export default function Main() {
                 <h2 className="text-textbase text-xl mt-2 sm:text-2xl sm:mt-4">{translator('role')}</h2>
                 <p className="test-textbase text-sm sm:text-base mt-3 text-justify sm:mt-5">{translator('workDescription')}</p>
                 <p className="text-textbase text-sm sm:text-base mt-3">{translator('personalTime')}</p>
-                <button className="text-mainact border-mainact border-3 py-2 px-4 sm:py-3 sm:px-7 my-5 hover:bg-hover hover:cursor-pointer active:bg-hover">{translator('contactButton')}</button>
+                <button
+                    className="text-mainact border-mainact border-3 py-2 px-4 sm:py-3 sm:px-7 my-5 hover:bg-hover hover:cursor-pointer active:bg-hover"
+                    onClick={() => setIsModalOpen(true)}
+                >
+                    {translator('contactButton')}
+                </button>
             </article>
         </section>
         <section className="flex justify-between flex-col lg:flex-row">
             <Skills />
             <Projects />
         </section>
+        <SendEmailModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+
     </main>
 }
